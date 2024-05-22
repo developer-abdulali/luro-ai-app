a32c3798f8125b603e770e29ff86ed77f5de12f3
developer-abdulali <ab.ali.soomro@gmail.com>
2024-05-22 22:53:12 +0500

    added changes

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,16 @@ import express from "express";
 import globalErrorsHandler from "./middlewares/globalErrorHandler";
 import userRouter from "./users/userRouter";
 import bookRouter from "./book/bookRouter";
+import cors from "cors";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+  })
+);
 app.use(express.json());
 
 app.get("/", (req, res, next) => {
